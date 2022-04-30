3732e48b0c3928389b0ebbab1eabb326bddf7fc9
mitcareem <mitcareem@gmail.com>
2022-04-30 11:11:31 +0530

    Completed Week 4 Assignments

---
+++ Week-4/index4.1.js
@@ -0,0 +1,11 @@
+const getName = () =>
+    new Promise((res, rej) => {
+        var num = Math.round(Math.random() * 100);
+        if (num % 5 == 0) rej(num);
+        else res(num);
+    });
+
+getName()
+    .then((res) => console.log(`Random Number is: ${res}`))
+    .catch((rej) => console.log(`No is divisible by 5: ${rej}`))
+    .finally(() => console.log(`Promise Handled`));
